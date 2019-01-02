b88c4c35474377671f8145ad2fb08acc92fb9648
Gabriella <csgabka@users.noreply.github.com>
2019-01-02 16:09:03 +0000

    Updates

---
+++ precache-manifest.55f2ec604f4d37966ef6cf04042b6305.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "086f3d6eb4a6d5e316a766be3a26d6fc",
+    "url": "/pomodoroClock/static/media/tom.086f3d6e.png"
+  },
+  {
+    "revision": "1d16cfe44b2b51316bc675e69b1ee9ac",
+    "url": "/pomodoroClock/static/media/bg2.1d16cfe4.jpg"
+  },
+  {
+    "revision": "fdd7de5b82e0a7b8b1ec",
+    "url": "/pomodoroClock/static/js/runtime~main.fdd7de5b.js"
+  },
+  {
+    "revision": "3061507e42c49597fd41",
+    "url": "/pomodoroClock/static/js/main.3061507e.chunk.js"
+  },
+  {
+    "revision": "f9aad8a1773c26c9094e",
+    "url": "/pomodoroClock/static/js/1.f9aad8a1.chunk.js"
+  },
+  {
+    "revision": "3061507e42c49597fd41",
+    "url": "/pomodoroClock/static/css/main.6939310b.chunk.css"
+  },
+  {
+    "revision": "ab98accead15ce5daaef6140d6f8f954",
+    "url": "/pomodoroClock/index.html"
+  }
+];
